9bba124bcae88e91c1d29853c99b4ecfda5f2fb6
jorgest7 <jorge.segovia.tormo@gmail.com>
2021-03-14 21:03:18 +0100

    add stylelint base

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,26 +10,26 @@
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 
-  
-    var btnNavbar = document.getElementById("closeNav");
-    var navbar = document.getElementById("navbar");
-    var showNavBar = document.getElementById("openNav");
 
-    btnNavbar.addEventListener('click', () => {
-      navbar.className = "hide";
-      showNavBar.className = "show openNav";
-    })
+  var btnNavbar = document.getElementById("closeNav");
+  var navbar = document.getElementById("navbar");
+  var showNavBar = document.getElementById("openNav");
 
-    showNavBar.addEventListener('click', () => {
-      navbar.className = "show navigation openTransition";
-      showNavBar.className = "hide";
-    })
+  btnNavbar.addEventListener('click', () => {
+    navbar.className = "hide";
+    showNavBar.className = "show opennav";
+  })
 
-  
-} )();
+  showNavBar.addEventListener('click', () => {
+    navbar.className = "show navigation opentransition";
+    showNavBar.className = "hide";
+  })
+
+
+})();
 
 
